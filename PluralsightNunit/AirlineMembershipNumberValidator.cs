637531eca19d16f1e601f1fcf5e319d1cfcb8429
Dávid Németh <dt-dave@hotmail.com>
2017-05-31 05:08:10 +0200

    Initial Commit

---
+++ PluralsightNunit/AirlineMembershipNumberValidator.cs
@@ -0,0 +1,30 @@
+﻿namespace PluralsightNunit
+{
+    public class AirlineMembershipNumberValidator
+    {
+        public bool ValidityOf(string membershipNumber)
+        {
+            var isIncorrectLength = membershipNumber == "" || membershipNumber.Length != 8;
+
+            if (isIncorrectLength)
+            {
+                return false;
+            }
+
+            if (StartsWithALetterFollowBy7Numbers(membershipNumber))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private static bool StartsWithALetterFollowBy7Numbers(string membershipNumber)
+        {
+            int dummy;
+
+            return char.IsLetter(membershipNumber[0]) &&
+                int.TryParse(membershipNumber.Substring(1), out dummy);
+        }
+    }
+}
---
+++ UnitsTests/AirlineMembershipNumberValidatorTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using PluralsightNunit;
+
+namespace UnitsTests
+{
+    [TestFixture]
+    public class AirlineMembershipNumberValidatorTests
+    {
+        [Test]
+        public void ShouldValidateCorrectNumber()
+        {
+            var sut = new AirlineMembershipNumberValidator();
+
+            var isValid = sut.ValidityOf("A1234567");
+
+            Assert.That(isValid, Is.True);
+        }
+    }
+}
