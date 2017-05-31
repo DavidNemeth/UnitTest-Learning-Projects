c718491acaf4343c12e11f0aac3bb7e19338e947
Dávid Németh <dt-dave@hotmail.com>
2017-05-31 04:25:11 +0200

    Add project files.

---
+++ PluralsightNunit/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PluralsightNunit
+{
+    public class Class1
+    {
+    }
+}
