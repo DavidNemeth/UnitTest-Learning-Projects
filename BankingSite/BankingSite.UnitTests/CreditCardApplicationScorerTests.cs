1ee402bf8870d52131111371278fd869c138dd94
Dávid Németh <dt-dave@hotmail.com>
2017-05-31 07:57:45 +0200

    Demo initial

---
+++ BankingSite/BankingSite.UnitTests/CreditCardApplicationScorerTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+
+namespace BankingSite.UnitTests
+{
+    [TestFixture]
+    public class CreditCardApplicationScorerTests
+    {
+        [Test]
+        public void ShouldDeclineUnderAgeApplicant()
+        {
+         
+        }
+
+        [Test]
+        public void ShouldAskGatewayForCreditCheck()
+        {
+      
+        }
+
+        [Test]
+        public void ShouldAcceptCorrectAgedApplicantWithGoodCreditHistory()
+        {
+         
+        }
+    }
+}
